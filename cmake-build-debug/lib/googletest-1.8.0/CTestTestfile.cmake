7aa38fa3d187838b0c5f377dff035f2d4b066c2c
CrazyPassion <von.king2010@gmail.com>
2017-09-17 13:43:45 +0800

    clean

--- cmake-build-debug/lib/googletest-1.8.0/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/vonking/Documents/C++/code/ClionUnitTest/lib/googletest-1.8.0
-# Build directory: /Users/vonking/Documents/C++/code/ClionUnitTest/cmake-build-debug/lib/googletest-1.8.0
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- cmake-build-debug/lib/googletest-1.8.0/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/vonking/Documents/C++/code/ClionUnitTest/lib/googletest-1.8.0/googletest
-# Build directory: /Users/vonking/Documents/C++/code/ClionUnitTest/cmake-build-debug/lib/googletest-1.8.0/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
